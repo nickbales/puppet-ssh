e470a2aaccec405941e1abd05f9493b8f8cbe198
Nick Bales <nick.bales@rackspace.com>
2018-11-09 22:23:01 +0000

    parameter introduction

--- manifests/service.pp
+++ manifests/service.pp
@@ -1,5 +1,8 @@
-class ssh::service {
-  service { 'sshd': 
+class ssh::service (
+  String $service_name = $::ssh::service_name,
+) {  
+  service { 'ssh-service': 
+    name       => $service_name,
     ensure     => running,
     enable     => true,
     hasstatus  => true,
